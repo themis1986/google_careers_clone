088e76de1ff65b47bf2f268ad66a892a1bdc5b57
thiakovidis <thiakovidis@agroapps.gr>
2022-10-25 15:19:34 +0300

    Populate navigation bar with links

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -6,6 +6,21 @@ describe("MainNav", () => {
     const wrapper = mount(MainNav);
     expect(wrapper.text()).toMatch("Bobo Careers");
   });
+  it("displays menu items for navigation", () => {
+    const wrapper = mount(MainNav);
+    const navigationMenuItems = wrapper.findAll(
+      "[data-test='main-nav-list-item']"
+    );
+    const navigationMenuTexts = navigationMenuItems.map((item) => item.text());
+    expect(navigationMenuTexts).toEqual([
+      "Teams",
+      "Locations",
+      "Life at Bobo",
+      "How we hire",
+      "Students",
+      "Jobs",
+    ]);
+  });
 });
 
 // describe("MainNav", async () => {
