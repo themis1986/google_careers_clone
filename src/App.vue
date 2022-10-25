088b87f794ec6499aaa4a848d6b8b57b82dc189c
thiakovidis <thiakovidis@agroapps.gr>
2022-10-25 11:44:31 +0300

    Add main navbar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
-  <h1 class="mt-8 text-6xl text-center">Hello Vue</h1>
+  <MainNav />
 </template>
 
 <script>
+import MainNav from "./components/MainNav.vue";
+
 export default {
   name: "App",
+  components: { MainNav },
 };
 </script>
 
