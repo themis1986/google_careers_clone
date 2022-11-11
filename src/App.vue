e7953a15b2af2687a107b8bb0c92d40e1ce14637
thiakovidis <thiakovidis@agroapps.gr>
2022-11-11 11:48:52 +0200

    Create hero component with starting grid, add functionality for dynamic changing color text, ad tests for exporting utility reusable nextElementInList  function

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
-  <MainNav />
+  <div class="">
+    <MainNav />
+    <Hero />
+  </div>
 </template>
 
 <script>
+import Hero from "./components/Hero.vue";
 import MainNav from "./components/MainNav.vue";
 
 export default {
   name: "App",
-  components: { MainNav },
+  components: { MainNav, Hero },
 };
 </script>
 
