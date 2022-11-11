920160127a92a8f91150d3e73047f4234c74561a
thiakovidis <thiakovidis@agroapps.gr>
2022-11-11 12:51:05 +0200

    Add test for the Headline.vue component

---
+++ tests/unit/components/Headline.test.js
@@ -0,0 +1,37 @@
+import { mount } from "@vue/test-utils";
+import { nextTick } from "vue";
+import Headline from "@/components/Headline.vue";
+
+describe("Headline", () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+  afterEach(() => {
+    jest.useRealTimers();
+  });
+
+  it("displays introductory action verb", () => {
+    const wrapper = mount(Headline);
+    const actionPhrase = wrapper.find("[data-test='action-phrase']");
+    expect(actionPhrase.text()).toBe("Build for everyone");
+  });
+
+  it("changes action verb at a consistent interval", () => {
+    mount(Headline);
+    expect(setInterval).toHaveBeenCalled();
+  });
+
+  it("swaps action verb after first interval", async () => {
+    const wrapper = mount(Headline);
+    jest.runOnlyPendingTimers();
+    await nextTick();
+    const actionPhrase = wrapper.find("[data-test='action-phrase']");
+    expect(actionPhrase.text()).toBe("Create for everyone");
+  });
+
+  it("removes interval when component disappears", () => {
+    const wrapper = mount(Headline);
+    wrapper.unmount();
+    expect(clearInterval).toHaveBeenCalled();
+  });
+});
