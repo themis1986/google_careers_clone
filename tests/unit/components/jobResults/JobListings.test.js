63381e2a4fe5dd1c39af7695c28a6e530c627de1
thiakovidis <thiakovidis@agroapps.gr>
2022-11-25 21:38:59 +0200

    Refactor job listings tests

--- tests/unit/components/jobResults/JobListings.test.js
+++ tests/unit/components/jobResults/JobListings.test.js
@@ -4,39 +4,36 @@ import JobListings from "@/components/jobResults/JobListings.vue";
 jest.mock("axios");
 
 describe("JobListings", () => {
+  const createRoute = (queryParams = {}) => ({
+    query: {
+      page: "5",
+      ...queryParams,
+    },
+  });
+  const createConfig = ($route) => ({
+    global: {
+      mocks: {
+        $route,
+      },
+    },
+  });
+
   it("fetches jobs", () => {
     axios.get.mockResolvedValue({ data: [] });
-    const $route = {
-      query: {
-        page: "5",
-      },
-    };
-    shallowMount(JobListings, {
-      global: {
-        mocks: {
-          $route,
-        },
-      },
-    });
+    const $route = createRoute();
+    shallowMount(JobListings, createConfig($route));
+
     expect(axios.get).toHaveBeenCalledWith("http://localhost:3000/jobs");
   });
 
   it("creates a job listing for a maximum of 10 jobs", async () => {
     axios.get.mockResolvedValue({ data: Array(15).fill({}) });
-    const $route = {
-      query: {
-        page: "1",
-      },
-    };
-    const wrapper = shallowMount(JobListings, {
-      global: {
-        mocks: {
-          $route,
-        },
-      },
-    });
+    const queryParams = { page: "1" };
+    const $route = createRoute(queryParams);
+    const wrapper = shallowMount(JobListings, createConfig($route));
     await flushPromises();
     const jobListings = wrapper.findAll("[data-test='job-listing']");
+
     expect(jobListings).toHaveLength(10);
   });
 });
