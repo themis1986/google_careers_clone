2d9e14603478c179e22a778d49d5fe7de05a42d1
thiakovidis <thiakovidis@agroapps.gr>
2022-11-25 13:07:14 +0200

    Refactor job list results in reusable list item components

--- src/components/jobResults/JobListings.vue
+++ src/components/jobResults/JobListings.vue
@@ -1,53 +1,21 @@
 <template>
   <main class="flex-auto p-8 bg-brand-gray-2">
     <ol>
-      <li class="mb-7">
-        <router-link
-          to="/jobs/results/1"
-          class="block mx-auto bg-white border border-solid border-brand-gray-2 rounded hover:shadow-gray"
-        >
-          <div class="pt-5 pb-2 mx-8 border-b border-solid border-brand-gray-2">
-            <h2 class="mb-2 tex-2xl">
-              Technical Program Manager, Perception, Augmented Reality
-            </h2>
-            <div class="flex flex-row align-middle">
-              <div class="mr-5">
-                <span>Bobo</span>
-              </div>
-              <div class="">
-                <span>San Fransisco, CA, USA</span>
-              </div>
-            </div>
-          </div>
-          <div class="px-8 py-4">
-            <div class="">
-              <h3 class="mt-1 mb-2">Qualifications</h3>
-              <div class="">
-                <ul class="pl-8 list-disc">
-                  <li>Barchelor's degree or equivalent practice experience.</li>
-                  <li>5 years of xperience in programm management.</li>
-                  <li>
-                    Experience analyzing data through querying database (e.g.
-                    SQL), using spreadsheet, and creating statistical models.
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div class="mt-2 text-center">
-              <router-link to="/jobs/results/1" class="text-brand-blue-1"
-                >Expand</router-link
-              >
-            </div>
-          </div>
-        </router-link>
-      </li>
+      <JobListing />
+      <JobListing />
+      <JobListing />
     </ol>
   </main>
 </template>
 
 <script>
+import JobListing from "@/components/jobResults/JobListing.vue";
+
 export default {
   name: "JobListings",
+  components: {
+    JobListing,
+  },
 };
 </script>
 
