6ee707ece98a0b382d6a6d19adb5c402993bb2ab
thiakovidis <thiakovidis@agroapps.gr>
2022-11-25 11:14:32 +0200

    Add test for submitting job-search form

--- src/components/jobSearch/JobSearchForm.vue
+++ src/components/jobSearch/JobSearchForm.vue
@@ -1,12 +1,17 @@
 <template>
   <form
     class="flex items-center w-full h-12 mt-14 border border-solid border-brand-gray-3 rounded-3xl"
+    @submit.prevent="searchForJobs"
   >
     <font-awesome-icon :icon="['fas', 'search']" class="ml-4 mr-3" />
     <div class="flex flex-nowrap flex-1 h-full text-base font-light">
       <div class="relative flex items-center flex-1 h-full pr-3">
         <label class="absolute left-0 -top-10" for="">Role</label>
-        <TextInput v-model="role" placeholder="Softare engineer" />
+        <TextInput
+          v-model="role"
+          placeholder="Softare engineer"
+          data-test="role-input"
+        />
       </div>
       <span
         class="flex items-center h-full px-3 border-l border-r border-brand-gray-3 bg-brand-gray-2"
@@ -14,11 +19,20 @@
       >
       <div class="relative flex items-center flex-1 h-full pl-3">
         <label class="absolute left-0 -top-10" for="">Where?</label>
-        <TextInput v-model="location" placeholder="Los Angeles" />
+        <TextInput
+          v-model="location"
+          placeholder="Los Angeles"
+          data-test="location-input"
+        />
       </div>
     </div>
 
-    <ActionButton text="Search" type="secondary" class="rounded-r-3xl" />
+    <ActionButton
+      text="Search"
+      type="secondary"
+      class="rounded-r-3xl"
+      data-test="form-submit-button"
+    />
   </form>
 </template>
 
@@ -39,11 +53,14 @@ export default {
     };
   },
   methods: {
-    updateRole($event) {
-      this.role = $event;
-    },
-    updateLocation($event) {
-      this.location = $event;
+    searchForJobs(event) {
+      this.$router.push({
+        name: "JobResults",
+        query: {
+          role: this.role,
+          location: this.location,
+        },
+      });
     },
   },
 };
