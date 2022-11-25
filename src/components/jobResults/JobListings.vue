541152e27c62e27471ae59d94ccd5b284812160a
thiakovidis <thiakovidis@agroapps.gr>
2022-11-25 13:37:49 +0200

    Init mock server , fetch jobs data in job listings component

--- src/components/jobResults/JobListings.vue
+++ src/components/jobResults/JobListings.vue
@@ -1,20 +1,35 @@
 <template>
   <main class="flex-auto p-8 bg-brand-gray-2">
     <ol>
-      <JobListing />
-      <JobListing />
-      <JobListing />
+      <JobListing v-for="job in jobs" :key="job.id" :job="job" />
     </ol>
   </main>
 </template>
 
 <script>
+import axios from "axios";
 import JobListing from "@/components/jobResults/JobListing.vue";
 
 export default {
   name: "JobListings",
   components: {
     JobListing,
+  },
+  data() {
+    return {
+      jobs: [],
+    };
+  },
+  mounted() {
+    this.fetchJobs();
+  },
+  methods: {
+    async fetchJobs() {
+      const response = await axios.get("http://localhost:3000/jobs");
+      const fetchedJobs = response.data;
+      this.jobs = fetchedJobs;
+      console.log(this.jobs);
+    },
   },
 };
 </script>
--- src/playground.js
+++ src/playground.js
@@ -9,3 +9,9 @@
 // export const multiply = (n_1, n_2) => {
 //   return n_1 * n_2;
 // };
+
+// const axios = require("axios");
+
+// const url = "http://localhost:3000/jobs";
+
+// axios.get(url).then((res) => console.log(res.data));
