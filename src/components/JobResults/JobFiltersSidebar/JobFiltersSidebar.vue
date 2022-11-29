acbb54a4d971d9be835ff380874fd6946574733f
thiakovidis <thiakovidis@agroapps.gr>
2022-11-29 15:20:46 +0200

    Extract organizations accordion to separate component

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
@@ -13,40 +13,18 @@
       <accordion header="Job types">
         <p>Placeholder for now</p>
       </accordion>
-      <accordion header="Organizations">
-        <div class="mt-5">
-          <fieldset>
-            <ul class="flex flex-row flex-wrap">
-              <li class="w-1/2 h-8">
-                <input id="VueTube" type="checkbox" class="mr-3" />
-                <label for="VueTube">VueTube</label>
-              </li>
-              <li class="w-1/2 h-8">
-                <input id="Between Vue" type="checkbox" class="mr-3" />
-                <label for="Between Vue">Between Vue</label>
-              </li>
-              <li class="w-1/2 h-8">
-                <input id="Et Vue Brute" type="checkbox" class="mr-3" />
-                <label for="Et Vue Brute">Et Vue Brute</label>
-              </li>
-              <li class="w-1/2 h-8">
-                <input id="Vue and a Half Men" type="checkbox" class="mr-3" />
-                <label for="Vue and a Half Men">Vue and a Half Men</label>
-              </li>
-            </ul>
-          </fieldset>
-        </div>
-      </accordion>
+      <JobFiltersSidebarOrganizations />
     </section>
   </div>
 </template>
 
 <script>
 import ActionButton from "@/components/Shared/ActionButton.vue";
+import JobFiltersSidebarOrganizations from "./JobFiltersSidebarOrganizations.vue";
 import Accordion from "@/components/Shared/Accordion.vue";
 
 export default {
   name: "JobFiltersSidebar",
-  components: { ActionButton, Accordion },
+  components: { ActionButton, Accordion, JobFiltersSidebarOrganizations },
 };
 </script>
